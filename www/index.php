73166f855cbd87288edb5b1d54d405720f615359
Jean-Luc Bonnet <lucbonnet10@gmail.com>
2020-09-28 13:05:37 -0300

    Introdução HTML

---
+++ www/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' conten='width=device-width, initial-scale=1.0'>
+  <meta name='robots' content='index, follow'>
+  <link rel='shortcut icon' href='http://www.ifto.edu.br/++theme++azul/img/favicon.ico'>
+  <link rel='stylesheet' href=''>
+  <title>Aprendendo HTML</title>
+</head>
+
+<body>
+  <h1>Título 1</h1>
+  <h2>Título 2</h2>
+  <h3>Título 3</h3>
+  <h4>Título 4</h4>
+  <h5>Título 5</h5>
+
+  <p>Parágrafo</p>
+
+  <h2>Listas</h2>
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+    <li>Item 5</li>
+  </ul>
+  <!-- <ol reversed> -->
+  <!-- <ol start='5'> -->
+  <!-- <ol type='I'> -->
+  <!-- <ol type='a'> -->
+  <ol type='a'>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <ul>
+      <li>Sub-Item</li>
+      <li>Sub-Item</li>
+    </ul>
+    <li>Item 5</li>
+  </ol>
+
+  <a title="Campus Araguaina" alt="Campus Araguaina" target="blank" href="http://www.ifto.edu.br/araguaina">
+    Link
+  </a>
+
+  <script src=''></script>
+</body>
+
+</html>
