0fb1cfd06770d0b2ef8624d05de10b1382efd51d
Jean-Luc Bonnet <lucbonnet10@gmail.com>
2020-10-14 12:39:08 -0300

    Atualizacao

---
+++ www/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' conten='width=device-width, initial-scale=1.0'>
+    <meta name='robots' content='index, follow'>
+    <link rel='shortcut icon' href='assets/img/ifto.svg'>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            Seja Bem Vendo ao meu site
+        </h1>
+        <p>Exemplo de seletores</p>
+    </header>
+
+    <main>
+        <section class="main_blog">
+            <header>
+                <h2>
+                    Sessão principal do Conteúdo
+                </h2>
+            </header>
+
+            <article>
+                <header>
+                    <h3>
+                        Teste de Artigo
+                    </h3>
+                </header>
+
+                <p>
+                    Ipsum temporibus dolore eligendi harum quidem omnis quia adipisci. Rerum quod nulla. In impedit
+                    consectetur quis ut. Sequi dolores sit natus accusantium et incidunt molestias. Quia velit velit
+                    assumenda. Vitae delectus maxime sed eveniet pariatur id.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>
+                        Teste de Artigo
+                    </h3>
+                </header>
+
+                <p>
+                    Ipsum temporibus dolore eligendi harum quidem omnis quia adipisci. Rerum quod nulla. In impedit
+                    consectetur quis ut. Sequi dolores sit natus accusantium et incidunt molestias. Quia velit velit
+                    assumenda. Vitae delectus maxime sed eveniet pariatur id.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>
+                        Teste de Artigo
+                    </h3>
+                </header>
+
+                <p>
+                    Ipsum temporibus dolore eligendi harum quidem omnis quia adipisci. Rerum quod nulla. In impedit
+                    consectetur quis ut. Sequi dolores sit natus accusantium et incidunt molestias. Quia velit velit
+                    assumenda. Vitae delectus maxime sed eveniet pariatur id.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>
+                        Teste de Artigo
+                    </h3>
+                </header>
+
+                <p>
+                    Ipsum temporibus dolore eligendi harum quidem omnis quia adipisci. Rerum quod nulla. In impedit
+                    consectetur quis ut. Sequi dolores sit natus accusantium et incidunt molestias. Quia velit velit
+                    assumenda. Vitae delectus maxime sed eveniet pariatur id.
+                </p>
+            </article>
+        </section>
+    </main>
+
+    <article>
+        <header>
+            <h3>
+                Teste de Artigo
+            </h3>
+        </header>
+
+        <p>
+            Ipsum temporibus dolore eligendi harum quidem omnis quia adipisci. Rerum quod nulla. In impedit
+            consectetur quis ut. Sequi dolores sit natus accusantium et incidunt molestias. Quia velit velit
+            assumenda. Vitae delectus maxime sed eveniet pariatur id.
+        </p>
+    </article>
+
+    <p>
+        Isso aqui é um bloco
+    </p>
+    <script></script>
+</body>
+
+</html>
